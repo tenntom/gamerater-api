c1df27b18763c1560f247b461c09840fcbb4c35e
Tom Clouse <tommyclouse@yahoo.com>
2021-08-03 14:12:00 -0500

    second commit

--- gamerater/asgi.py
+++ gamerater/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for raterapi project.
+ASGI config for gameraterapi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'raterapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamerater.settings')
 
 application = get_asgi_application()
